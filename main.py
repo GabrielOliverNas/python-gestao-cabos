9112917b145f1920f172124b668be34a29bcb132
gabriel.nascimento <4357#Joaogu>
2022-08-25 17:23:53 -0300

    Implementação de querys para testar a base

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ try:
     print('-------------------')
     connection = mysql.connector.connect(
         host='localhost',
-        database='teste01',
+        database='projeto-faculdade',
         user='root',
         password='')
 
@@ -22,20 +22,21 @@ try:
         # connection.commit()
         # print('number of rows deleted', cursor.rowcount)
 
-        # Insert a record
-        # sql_insert_query = "insert into laptop (id, name, price) VALUES (1, 'Marcos', 2.0)"
+        # Insert a record into CABOS
+        # sql_insert_query = "INSERT INTO cabos(idCabo, nome, tamanho, cor, dataEntradaCabo, vidaUtil) VALUES ('312312','HDMI', 2.0,'preto', NOW(),'60')"
         # cursor.execute(sql_insert_query)
         # connection.commit()
-        print('Insert na base')
+        # print('Insert na base')
 
-        # Select byId a record
-        # sql_select_Query = "select * from Laptop where name = 'Lenovo ThinkPad P71'"
-        # cursor.execute(sql_select_Query)
-        # records = cursor.fetchall()
-        # print('records: ', records)
+        # Insert a record into FUNCIONARIOS
+        # sql_insert_query = "INSERT INTO funcionario(idFuncionario, nome, telefone) VALUES ('xy.98-Ah 77','Josiano Robinsom Mathias','(81)9 1818-1919')"
+        # cursor.execute(sql_insert_query)
+        # connection.commit()
+        # print('Insert na base')
 
         # Select all a record
-        # cursor.execute('select * from Laptop')
+        # select_join_table = "SELECT fn.nome, fn.telefone, cb.nome, cb.tamanho FROM statuscabos sc JOIN funcionario fn ON sc.idFuncionario = fn.idFuncionario JOIN cabos cb ON sc.idCabo = cb.idCabo"
+        # cursor.execute(select_join_table)
         # records = cursor.fetchall()
         # print('Registros na base: ', records)
 
